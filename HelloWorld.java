496da3ab0b186ddfdab8327bc30c18df9ea11114
Ammara Agsalud <aagsalud@student.sdccd.edu>
2026-02-08 17:12:05 -0800

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * This is my first java app, It Prints out Hello 
+ * World and my name to the console,
+ * @author Ammara Agsalud, aagsalud@student.sdccd.edu
+ * @since 02/08/2026
+ * @version v1.0
+ */
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Ammara Agsalud");
+    }
+}
