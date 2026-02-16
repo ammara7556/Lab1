48c8399f3eb77b2efea4a98fabd89e75082ac62c
ammara7556 <aagsalud@student.sdccd.edu>
2026-02-15 20:00:52 -0800

    Delete GetToKnowYou.java

--- GetToKnowYou.java
+++
@@ -1,5 +0,0 @@
-public class GetToKnowYou {
-    public static void main(String[] args) {
-        System.out.println("Ammara Agsalud\nMy major is Cognitive Science\nI enjoy working out and anytype of creation through art");
-    }
-}
