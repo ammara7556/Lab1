08742ab143b0c3cd61080e31deca53c834b3dd6d
ammara7556 <aagsalud@student.sdccd.edu>
2026-02-15 20:05:57 -0800

    Lab2 Part 1

---
+++ GetToKnowYou.java
@@ -0,0 +1,5 @@
+public class GetToKnowYou {
+    public static void main(String[] args) {
+        System.out.println("Ammara Agsalud\nMy major is Cognitive Science\nI enjoy working out and anytype of creation through art");
+    }
+}
